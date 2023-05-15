8b83bdfa15942cf2745885390c1c6fb53ea2f5ab
Olegs <ole6ka4@inbox.lv>
2023-05-15 17:51:28 +0300

    Add pipiline base for Jenkins

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
 const app = express();
 const PORT = 1010;
 
 // Dummy books data
 const books = [
+
   { id: 1, title: 'The Catcher in the Rye', author: 'J.D. Salinger', pageCount: 224, country: 'United States' },
   { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee', pageCount: 281, country: 'United States' },
   { id: 3, title: '1984', author: 'George Orwell', pageCount: 328, country: 'United Kingdom' },
@@ -13,12 +13,10 @@ const books = [
   { id: 6, title: 'Astoņi Kustiņi', author: 'I. Ziedonis', pageCount: 180, country: 'Latvia' },
   { id: 7, title: 'Chip War', author: '..', pageCount: 300, country: 'USA' },
 ];
-
 // Endpoint to get list of dummy books
 app.get('/books', (req, res) => {
   res.json(books);
 });
-
 // Endpoint to get a book by ID
 app.get('/books/:id', (req, res) => {
   const id = req.params.id;
@@ -29,13 +27,10 @@ app.get('/books/:id', (req, res) => {
     res.status(404).json({ message: 'Book not found' });
   }
 });
-
 // Default port
 const defaultPort = 1050;
-
 // Get port from command-line argument or use default port
 const port = process.argv[2] || defaultPort;
-
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
